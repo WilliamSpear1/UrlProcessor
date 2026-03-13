da637cb9e59ed0de429ba86385d7bdfd4791a4db
William Spearman <spearmanwm@gmail.com>
2026-03-13 14:34:14 -0400

    Updated - Logging Configuration.

--- conf/logger_conf.py
+++ conf/logger_conf.py
@@ -1,60 +1,34 @@
 #Ensure the logs directory exists
-import datetime
-import gzip
+import atexit
 import json
 import logging
 import logging.config
 import os
-import shutil
-from logging import Logger
-from logging.handlers import TimedRotatingFileHandler
-from pathlib import Path
 
-Path("/var/log/url_processor").mkdir(parents=True, exist_ok=True)
-environment = os.environ.get('ENVIRONMENT')
+def safe_load_config() -> logging.Logger | None:
+    environment = os.environ.get("ENVIRONMENT")
 
-# Compressor
-"""
-:param source -> path to rotated log file.
-:para dest -> destination filename without the .gz extension
-
-Adds the gz extension to file
-"""
-def compress_rotated_log(source, dest) -> None:
-    #rb -> read binary mode
-    #wb -> write binary mode
-    #f_in -> file object that is being read from
-
-    with open(source, 'rb') as f_in, gzip.open(dest, 'wb') as f_out:
-        shutil.copyfileobj(f_in, f_out)
-
-    if os.path.exists(source):
-        os.remove(source)
-
-def add_rotator(logger) -> None:
-    for handler in logger.handlers:
-        if isinstance(handler, TimedRotatingFileHandler):
-            handler.rotator = compress_rotated_log
-            handler.namer = lambda name: name + '.gz'
-
-def safe_load_config() -> Logger | None:
     try:
         config_file = f'/app/logs/logging_config_{environment}.json'
         with open(config_file, 'r') as f:
             config = json.load(f)
         logging.config.dictConfig(config)
+        queue_handler = logging.getHandlerByName("queue_handler")
+        if queue_handler:
+            queue_handler.listener.start()
+            atexit.register(queue_handler.listener.stop)
         return logging.getLogger()
     except (FileNotFoundError, json.JSONDecodeError) as e:
         logging.basicConfig(level=logging.DEBUG)
         logging.warning("Config file not found, using basicConfig with DEBUG level.")
         return logging.getLogger()
 
+
 def setup_logging(name) -> logging.Logger:
     """
     Set up logging configuration.
     """
     logger = safe_load_config()
-    add_rotator(logger)
     logger.info(f"Logging is set up in: {name}")
 
     return logger
